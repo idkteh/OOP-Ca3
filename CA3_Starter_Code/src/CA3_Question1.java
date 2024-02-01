6ae35f5c2de8ce25fb9a1837aab92d846bb1e0ac
idkteh <124367770+idkteh@users.noreply.github.com>
2024-02-01 15:50:53 +0000

    more stuff

--- CA3_Starter_Code/src/CA3_Question1.java
+++ CA3_Starter_Code/src/CA3_Question1.java
@@ -1,6 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Stack;
+import java.util.Scanner;
 
 /**
  *  Name:Ida Tehlarova
@@ -15,7 +16,32 @@ public class CA3_Question1
     }
 
     public void parkingStack1 (){
-        Deque<String> myStack = new ArrayDeque<>();
+        Deque<Integer> street = new ArrayDeque<>();
+        Deque<Integer> parking = new ArrayDeque<>();
+
+        System.out.println("Write positive and negative numbers to add od remove numbers, \"0\" for exit");
+
+        Scanner in = new Scanner(System.in);
+        int i = in.nextInt();
+        while(i!=0){
+            if (i<0){
+               int remove = Math.abs(i);
+               int move = street.peek();
+               while (move!= remove-1)
+               {
+                   street.pop();
+                   parking.push(move);
+                   move =  move-1;
+               }
+               i = in.nextInt();
+            }else{
+                street.push(i);
+                i= in.nextInt();
+            }
+        }
+        System.out.println(street);
+        System.out.println(parking);
+
     }
 
     public static void main(String[] args) {
--- CA3_Starter_Code/src/CA3_Question3.java
+++ CA3_Starter_Code/src/CA3_Question3.java
@@ -1,6 +1,9 @@
 import java.io.FileNotFoundException;
 import java.util.HashSet;
 import java.util.TreeMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.io.File;
 /**
  *  Name: Ida Tehlarova
  *  Class Group: GD2b
@@ -8,26 +11,32 @@ import java.util.TreeMap;
 
 
 // use hashmap
-    // if we use treemap the items are going to be in ascending order when we take them out
+// if we use treemap the items are going to be in ascending order when we take them out
 public class CA3_Question3
 {
-    public void main(){
-        int x = 4;
-    }
-    public static void readFile(String fileName)
-    {
-    TreeMap<String,HashSet<Integer>> map = new TreeMap<>();
-    if (map.contains(token)){
-        map.get(token).add(lineNumber);
-    }else{
-        HashSet <Integer> set = new HashSet<>();
-        set.add("ln number");
-        map.put(token,set);
-    }
-
-    }
-
     public static void main(String[] args) throws FileNotFoundException {
-        readFile("src/CA3_Question1.java");
+        Map<String, HashSet<Integer>> map = new TreeMap<>();
+        Scanner fin = new Scanner(new File("CA3_Starter_Code/src/CA3_Question1.java"));
+        int lineNumber = 1;
+        while (fin.hasNextLine()) {
+            Scanner in = new Scanner(fin.nextLine());
+            in.useDelimiter("[^A-Za-z0-9]");
+            while (in.hasNext()) {
+                String token = in.next();
+                if (map.containsKey(token)) {
+                    map.get(token).add(lineNumber);
+                } else {
+                    HashSet<Integer> set = new HashSet<>();
+                    set.add(lineNumber);
+                    map.put(token, set);
+                }
+            }
+            lineNumber++;
+        }
+        System.out.println(map);
     }
 }
+
+
+
+
